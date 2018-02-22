174856bf5248183879472fb8fb8847d032bb98d2
boda <abdelrahmanmustaf00@gmail.com>
2018-02-22 15:29:38 +0200

    add the full version of the adding effects to the selecvted image from gallary.

--- app/src/main/java/com/indeves/selfieapp/EffectActivity.java
+++ app/src/main/java/com/indeves/selfieapp/EffectActivity.java
@@ -2,18 +2,24 @@ package com.indeves.selfieapp;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.zomato.photofilters.SampleFilters;
 import com.zomato.photofilters.imageprocessors.Filter;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 import java.util.List;
 
 public class EffectActivity extends AppCompatActivity implements ThumbnailCallback {
@@ -22,24 +28,37 @@ public class EffectActivity extends AppCompatActivity implements ThumbnailCallba
     }
 
     private Activity activity;
+    private static int RESULT_LOAD_IMAGE = 1;
     private RecyclerView thumbListView;
-    private ImageView placeHolderImageView;
-
+    private ImageView placeHolderImageView, select;
+    Bitmap selectImage;
+    Bitmap image;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         activity = this;
+
+
         initUIWidgets();
     }
-//initialize recyclerView containig the edits
+
+    //initialize recyclerView containig the edits
     private void initUIWidgets() {
+        select = (ImageView) findViewById(R.id.place_holder_select);
         thumbListView = (RecyclerView) findViewById(R.id.thumbnails);
         placeHolderImageView = (ImageView) findViewById(R.id.place_holder_imageview);
+        select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                select.setVisibility(View.GONE);
+                getImage();
+
+            }
+        });
         // get the original the image from gallary after that
-        placeHolderImageView.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getApplicationContext().getResources(), R.drawable.photo), 640, 640, false));
+        // placeHolderImageView.setImageBitmap(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getApplicationContext().getResources(), R.drawable.photo), 640, 640, false));
         // deploy the selected image overt the recyclerView items
-        initHorizontalList();
     }
 
     private void initHorizontalList() {
@@ -57,7 +76,7 @@ public class EffectActivity extends AppCompatActivity implements ThumbnailCallba
         Runnable r = new Runnable() {
             public void run() {
                 // we should get passed the selected photo to this method  to be edited
-                Bitmap thumbImage = Bitmap.createScaledBitmap(BitmapFactory.decodeResource(context.getResources(), R.drawable.photo), 640, 640, false);
+                Bitmap thumbImage = Bitmap.createScaledBitmap(selectImage, 640, 640, false);
                 ThumbnailItem t1 = new ThumbnailItem();
                 ThumbnailItem t2 = new ThumbnailItem();
                 ThumbnailItem t3 = new ThumbnailItem();
@@ -102,6 +121,39 @@ public class EffectActivity extends AppCompatActivity implements ThumbnailCallba
     // change the selected image over the original one to be viewed
     @Override
     public void onThumbnailClick(Filter filter) {
-        placeHolderImageView.setImageBitmap(filter.processFilter(Bitmap.createScaledBitmap(BitmapFactory.decodeResource(this.getApplicationContext().getResources(), R.drawable.photo), 640, 640, false)));
+        placeHolderImageView.setImageBitmap(filter.processFilter(Bitmap.createScaledBitmap(selectImage, 640, 640, false)));
+    }
+
+    private void getImage() {
+
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, RESULT_LOAD_IMAGE);
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resultCode, Intent data) {
+        super.onActivityResult(reqCode, resultCode, data);
+
+
+        if (resultCode == RESULT_OK) {
+            try {
+                final Uri imageUri = data.getData();
+                final InputStream imageStream = getContentResolver().openInputStream(imageUri);
+                selectImage = BitmapFactory.decodeStream(imageStream);
+                placeHolderImageView.setImageBitmap(Bitmap.createScaledBitmap(selectImage, 640, 640, false));
+                initHorizontalList();
+
+
+
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+                Toast.makeText(EffectActivity.this, "Something went wrong", Toast.LENGTH_LONG).show();
+            }
+
+        } else {
+            Toast.makeText(EffectActivity.this, "You haven't picked Image", Toast.LENGTH_LONG).show();
+        }
     }
 }
